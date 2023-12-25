cc166ddbe95a30453e3df266b5bee5824ce75786
Chris Puska <theta@hey.com>
2023-12-25 12:54:21 -0800

    Init public components

---
+++ Error/Error.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Heading from "../Heading";
+
+const Error = (props) => {
+	const errorCode: string | undefined = props.errorCode ? `${props.errorCode} ` : undefined;
+
+	return <Heading level={1}>{errorCode}Not found.</Heading>;
+};
+
+export default Error;
